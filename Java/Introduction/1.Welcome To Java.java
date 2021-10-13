2956e41f4079b4b63dd380f3b47dc79da02c35a1
Nisha Kedia <68768670+nisha290@users.noreply.github.com>
2021-10-13 12:37:17 +0530

    Update 1.Welcome To Java.java

--- Java/Introduction/1.Welcome To Java.java
+++ Java/Introduction/1.Welcome To Java.java
@@ -1,7 +1,4 @@
---------
-QUESTION
---------
-
+/*
 Welcome to the world of Java! In this challenge, we practice printing to stdout.
 
 The code stubs in your editor declare a Solution class and a main method. Complete the main method by copying the two lines of code below and pasting them inside the body of your main method.
@@ -23,9 +20,7 @@ Sample Output
 Hello, World.
 Hello, Java.
 
---------
-SOLUTION
---------
+*/
 
 import java.io.*;
 import java.util.*;
